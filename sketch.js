83176954fbffa6989aed4014a45dad5c86b40c21
MartinidotPanini <miali2005.dk@gmail.com>
2025-03-24 15:01:11 +0100

    Update sketch.js slutskærm

--- sketch.js
+++ sketch.js
@@ -1,48 +1,65 @@
 let pos1, vel1, acc1; // (WASD)
 let pos2, vel2, acc2; // (Arrow)
-
 let maxS; // maxSpeed
 let fri = 0.87; // friction
-
-let blobdata = []; // opbevare blobs
-
-let r = 15;//radius af blob
-let rr = 15;
-let rb = 15;
-redPlayerPoint = 0;
-bluePlayerPoint = 0;
-
-
+let blobdata = []; // store blobs
+let rr = 15; // radius of red player
+let rb = 15; // radius of blue player
+let redPlayerPoint = 0;
+let bluePlayerPoint = 0;
+let win = false;
 
 function setup() {
   createCanvas(600, 600);
-
-  pos1 = createVector(random(width - 570, width - 450), random(height - 570, height - 450)); // Vektor der bestemmer startposition for spiller 1
+startPos();
+ 
+}
+function startPos(){
+  background(220);
+   // Set initial positions
+  pos1 = createVector(random(30, 150), random(30, 150));
   vel1 = createVector(0, 0);
   acc1 = createVector(0, 0);
 
-  pos2 = createVector(random(width - 30, width - 150), random(height - 150, width - 30)); // Vektor der bestemmer startposition for spiller 1
+  pos2 = createVector(random(width - 150, width - 30), random(height - 150, height - 30));
   vel2 = createVector(0, 0);
   acc2 = createVector(0, 0);
 
+  // Generate blobs
   for (let i = 0; i < 60; i++) {
     let newblob = {
-      x: random(0, 600),
-      y: random(0, 600),
-      r,
+      x: random(12, width - 12),
+      y: random(12, height - 12),
+      r: Math.floor(random(9, 12)),
+      color: { r: random(0, 255), g: random(0, 255), b: random(0, 255) }
     };
     blobdata.push(newblob);
   }
+  
+  
 }
+
+
+
+
+
+
 
 function draw() {
   background(220);
-  WASD();
-  PIL();
-  BlobG();
+
+  if (!win) {
+    WASD();
+    PIL();
+    BlobG();
+    SpisB();
+  } else {
+    displayWinner();
+  }
 }
 
-function WASD(redPlayerPoint) {
+// Player 1 controls (WASD)
+function WASD() {
   acc1.set(0, 0);
 
   if (keyIsDown(87)) acc1.y = -0.7;
@@ -52,70 +69,101 @@ function WASD(redPlayerPoint) {
 
   vel1.add(acc1);
   vel1.mult(fri);
-  vel1.limit((maxS = 2.6/(2*sqrt(rr))*10));
+  vel1.limit((2.6 / (2 * sqrt(rr))) * 10);
   pos1.add(vel1);
 
   pos1.x = constrain(pos1.x, rr, width - rr);
   pos1.y = constrain(pos1.y, rr, height - rr);
-
+  stroke(20)
   fill(255, 0, 0);
- let redsize = ellipse(pos1.x, pos1.y, rr*2, rr*2);
-
+  ellipse(pos1.x, pos1.y, rr * 2, rr * 2);
 }
 
+// Player 2 controls (Arrow Keys)
 function PIL() {
   acc2.set(0, 0);
 
-  if (keyIsDown(38)) acc2.y = -0.7;
-  if (keyIsDown(40)) acc2.y = 0.7;
-  if (keyIsDown(37)) acc2.x = -0.7;
-  if (keyIsDown(39)) acc2.x = 0.7;
+  if (keyIsDown(UP_ARROW)) acc2.y = -0.7;
+  if (keyIsDown(DOWN_ARROW)) acc2.y = 0.7;
+  if (keyIsDown(LEFT_ARROW)) acc2.x = -0.7;
+  if (keyIsDown(RIGHT_ARROW)) acc2.x = 0.7;
 
   vel2.add(acc2);
   vel2.mult(fri);
-  vel2.limit((maxS= 2.6/(2*sqrt(rb))*10));
+  vel2.limit((2.6 / (2 * sqrt(rb))) * 10);
   pos2.add(vel2);
+
   pos2.x = constrain(pos2.x, rb, width - rb);
   pos2.y = constrain(pos2.y, rb, height - rb);
 
+  stroke(20)
   fill(0, 0, 255);
-  let bluesize = ellipse(pos2.x, pos2.y, rb*2, rb*2);
-  
+  ellipse(pos2.x, pos2.y, rb * 2, rb * 2);
 }
 
+// Blob rendering
 function BlobG() {
-  fill(0, 255, 0);
-  for (let i = blobdata.length - 1; i >= 0; i--) {
-    let blob = blobdata[i];
+  noStroke();
+  for (let i = 0; i < blobdata.length; i++) {
+    fill(blobdata[i].color.r, blobdata[i].color.g, blobdata[i].color.b);
+    ellipse(blobdata[i].x, blobdata[i].y, blobdata[i].r * 2);
+  }
+}
 
-    ellipse(blob.x, blob.y, blob.r * 2);
-    if (dist(pos1.x, pos1.y, blob.x, blob.y) < rr) {
+// Collision Detection & Score Update
+function SpisB() {
+  for (let i = blobdata.length - 1; i >= 0; i--) {
+    if (dist(pos1.x, pos1.y, blobdata[i].x, blobdata[i].y) < rr) {
       blobdata.splice(i, 1);
-      redPlayerPoint ++
-      console.log(redPlayerPoint);
-      rr += 1
-    
-    }
-    if (dist(pos2.x, pos2.y, blob.x, blob.y) < rb) {
+      redPlayerPoint ++ ;
+      console.log("Red Score: " + redPlayerPoint);
+      rr += 1;
+    } else if (dist(pos2.x, pos2.y, blobdata[i].x, blobdata[i].y) < rb) {
       blobdata.splice(i, 1);
-      bluePlayerPoint ++
-      console.log(bluePlayerPoint);
-      rb += 1
-      
-    }
-    if(dist(pos1.x,pos1.y,pos2.x,pos2.y) < rb + rr){
-      if(rr>rb){
-        rr += rb;
-        rb=0;
-      }
-      if(rb>rr){
-        rb +=rr;
-        rr=0;
-      }
+      bluePlayerPoint ++;
+      console.log("Blue Score: " + bluePlayerPoint);
+      rb += 1;
     }
   }
-      textSize(32);
-      fill(255,255,255);
-      text(redPlayerPoint,100,100); 
-      text(bluePlayerPoint,400,100); 
+
+  // Check Player Collision
+  if (dist(pos1.x, pos1.y, pos2.x, pos2.y) < rb- sqrt(rr) + rr - sqrt(rb)) {
+    if (rr > rb) {
+      win = "Red Wins!";
+    } else if (rb > rr) {
+      win = "Blue Wins!";
+    }
+  }
+
+  // Display scores
+  stroke(20)
+  textSize(32);
+  fill(255, 0, 0);
+  text(redPlayerPoint, 100, 100);
+  fill(0, 0, 255);
+  text(bluePlayerPoint, 400, 100);
 }
+
+// Display winner
+function displayWinner() {
+  background(50);
+  fill(255);
+  textSize(50);
+  textAlign(CENTER, CENTER);
+  text(win, width / 2, height / 2);
+  noLoop(); // Stop the game
+  let button = createButton("Restart");
+  button.mousePressed(startGame);
+}
+function startGame(){
+  startPos();
+  redPlayerPoint = 0;
+  bluePlayerPoint = 0;
+  win = false;
+  rr = 15; // radius of red player
+  rb = 15;
+  draw();
+}
+
+
+
